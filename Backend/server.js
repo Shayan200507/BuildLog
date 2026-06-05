55ef8ecc0b86a5937dffd2cbd436e3eb45cdf2c2
Shayan Sheikh <shayan.faisal1095@gmail.com>
2026-06-06 00:18:01 +0500

    setup complete

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,29 @@
 import express from "express"
-
+import session from "express-session";
+import dotenv from "dotenv"
 
 const server = express()
 const PORT = 8000
 
+dotenv.config()
+
+app.use(express.json())
+app.use(session({
+    secret: process.env.SPIRAL_SESSION_SECRET|| 'dev-secret' ,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax"
+    }
+
+}))
 
 
 
 
-
-
-server.listen(PORT,()=>{console})
+server.listen(PORT,()=>{console.log(`connected to port: ${PORT}` )})
 
 
